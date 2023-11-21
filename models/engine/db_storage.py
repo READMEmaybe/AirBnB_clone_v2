cab7cfdb5e9319c87ff24147bde6226b86bf4535
Blackarrow299 <yassine.goumni@gmail.com>
2023-11-21 21:58:57 +0100

    fix: task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,15 +7,18 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 class DBStorage:
+    """storage engine."""
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine(
-            f"mysql+mysqldb://{getenv('HBNB_MYSQL_USER')}:{getenv('HBNB_MYSQL_PWD')}@"
-            f"{getenv('HBNB_MYSQL_HOST')}/{getenv('HBNB_MYSQL_DB')}",
-            pool_pre_ping=True
-        )
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True
+                                      )
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
