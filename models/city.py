6555952c6a80daca10a5a67c09df62b093fa0a76
READMEmaybe <haroune11411@gmail.com>
2023-11-22 01:56:35 +0100

    Add task 7

--- models/city.py
+++ models/city.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy import Column, ForeignKey, String
 from models.base_model import Base
 from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
 
 
 class City(BaseModel, Base):
