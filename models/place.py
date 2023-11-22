2435ffd270388bfca495446a7bfbcd1aee191592
Blackarrow299 <yassine.goumni@gmail.com>
2023-11-22 22:08:49 +0100

    fix task 10

--- models/place.py
+++ models/place.py
@@ -12,6 +12,15 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 from models.amenity import Amenity
+from sqlalchemy import Table
+
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
