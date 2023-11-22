e6b246f595a5ed0fcde1631c39967931d57c5b03
READMEmaybe <haroune11411@gmail.com>
2023-11-22 01:44:12 +0100

    Testing fix for task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ db storage class"""
 from os import getenv
-from models.base_model import Base, Amenity, City, Place, Review, State, User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models.base_model import Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class DBStorage:
