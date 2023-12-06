7863fdb95b3428f36a7dff190f22c994a3a8322e
READMEmaybe <haroune11411@gmail.com>
2023-12-07 00:27:30 +0100

    revert

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,13 +5,7 @@ apt-get update
 apt-get -y install nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/current/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
