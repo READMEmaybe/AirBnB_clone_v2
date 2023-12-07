517a135437ed46c0cfdb6a361e01e354b2419a42
READMEmaybe <haroune11411@gmail.com>
2023-12-07 23:59:27 +0100

    Refactor

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,40 @@ apt-get update
 apt-get -y install nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+printf %s "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html
+
+	server_name _;
+
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+	}
+
+	location / {
+		add_header X-Served-By $HOSTNAME;
+		try_files \$uri \$uri/ @404;
+	}
+
+	location /redirect_me {
+		return 301 \"Moved Permanently\";
+	}
+
+	location @404 {
+		return 404 \"Ceci n'est pas une page\n\";
+	}
+}" > /etc/nginx/sites-available/default
+
 service nginx restart
